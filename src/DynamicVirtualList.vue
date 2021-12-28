2e2af9a658246796d01e8067700986c24f2fb8c7
Vahe Araqelyan <vahe.araqelyan.2013@hotmail.com>
2021-12-28 21:28:05 +0400

    use vue v3 api for DynamicVirtualList

--- src/DynamicVirtualList.vue
+++ src/DynamicVirtualList.vue
@@ -14,7 +14,7 @@
           :key="row.data[itemId]"
           @start="onStartDrag"
           :has-started="hasStarted"
-          :container="$refs.viewport"
+          :container="viewport"
           :index="start + index"
           :active-index="activeIndex"
           :new-index="newIndex"
@@ -48,10 +48,22 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import Move from './Move.vue'
 import Dragger from './Dragger.vue'
 import { DIRECTION } from './constants/props'
+import useAutoscroll from './hooks/useAutoscroll'
+import useDnD from './hooks/useDnD'
+import {
+  watch,
+  ref,
+  inject,
+  nextTick,
+  provide,
+  reactive,
+  computed,
+  onMounted,
+} from 'vue'
 
 Array.prototype.move = function (from, to) {
   this.splice(to, 0, this.splice(from, 1)[0])
@@ -63,256 +75,250 @@ const EVENT_OPTS = {
   capture: true,
 }
 
-export default {
-  name: 'dynamic-virtual-list',
-  props: {
-    list: Array,
-    viewport: Boolean,
-    itemId: String,
-    direction: {
-      type: String,
-      default: DIRECTION.COLUMN,
-    },
+const props = defineProps({
+  list: Array,
+  viewport: Boolean,
+  itemId: String,
+  direction: {
+    type: String,
+    default: DIRECTION.COLUMN,
   },
-  components: {
-    Move,
-    Dragger,
-  },
-  data: () => ({
-    bottom: 0,
-    top: 0,
-    start: 0,
-    end: 0,
-    viewportHeight: 0,
-    rows: [],
-    heightMap: [],
-    averageHeight: 0,
-    // ---
-    hasStarted: false,
-    newIndex: -1,
-    activeIndex: -1,
-    moveInstance: {},
-    offset: 0,
-  }),
-  mounted() {
-    if (this.viewport) {
-      this.resized()
-      window.addEventListener('resize', this.resized, EVENT_OPTS)
-      window.addEventListener('scroll', this.onScroll, EVENT_OPTS)
-    } else {
-      this.viewportHeight = this.$refs.viewport[this.getProp.offsetSize]
+})
+
+let bottom = ref(0)
+let top = ref(0)
+let start = ref(0)
+let end = ref(0)
+let viewportHeight = ref(0)
+let rows = ref([])
+
+let viewport = ref()
+let content = ref()
+
+let heightMap = reactive([])
+
+let averageHeight = ref(0)
+// ---
+let hasStarted = ref(false)
+let newIndex = ref(-1)
+let activeIndex = ref(-1)
+let moveInstance = reactive({})
+let offset = ref(0)
+
+onMounted(() => {
+  if (props.viewport) {
+    resized()
+    addEventListener('resize', resized, EVENT_OPTS)
+    addEventListener('scroll', onScroll, EVENT_OPTS)
+  } else {
+    viewportHeight.value = viewport.value[getProp.value.offsetSize]
+  }
+
+  rows.value = content.value.getElementsByClassName('vList__row')
+  initVisibleRows()
+})
+
+const initVisibleRows = async () => {
+  const scrollTop = viewport.value[getProp.value.scroll]
+
+  await nextTick()
+
+  let content_height = top.value - scrollTop
+  let i = start.value
+
+  while (content_height < viewportHeight.value && i < props.list.length) {
+    let row = rows.value[i - start.value]
+
+    if (!row) {
+      end.value = i + 1
+      await nextTick()
+      row = rows.value[i - start.value]
     }
 
-    this.rows = this.$refs.content.getElementsByClassName('vList__row')
-    this.initVisibleRows()
-  },
-  methods: {
-    resized() {
-      let height = window.innerHeight || document.documentElement.offsetHeight
-      if (height !== this.viewportHeight) {
-        this.viewportHeight = height
-      }
-    },
-    async onScroll() {
-      let scrollTop = 0
-      if (!this.viewport) {
-        scrollTop = this.$refs.viewport[this.getProp.scroll]
-      } else {
-        scrollTop = Math.max(
-          0,
-          -this.$refs.viewport.getBoundingClientRect().top || 0
-        )
-      }
+    const rowOffsetHeight = row[getProp.value.offsetSize]
 
-      this.offset = scrollTop
+    const row_height = (heightMap[i] = rowOffsetHeight)
 
-      const listLength = this.list.length
+    content_height += row_height
 
-      // Get start
-      let i = 0
-      let totalHeight = 0
+    i += 1
+  }
 
-      while (i < listLength) {
-        const rowHeight = this.heightMap[i] || this.averageHeight
+  const remaining = props.list.length - end.value
+  averageHeight.value = (top.value + content_height) / end.value
 
-        if (totalHeight + rowHeight > scrollTop) {
-          this.start = i
-
-          this.top = totalHeight
-          break
-        }
-
-        totalHeight += rowHeight
-        i += 1
-      }
-
-      while (i < listLength) {
-        if (this.heightMap[i]) {
-          totalHeight += this.heightMap[i]
-        } else if (this.rows[i]) {
-          totalHeight += this.rows[i][this.getProp.offsetSize]
-        } else {
-          this.end = i + 1
-          await this.$nextTick()
-
-          const rowOffsetHeight =
-            this.rows[i - this.start][this.getProp.offsetSize]
-
-          totalHeight += rowOffsetHeight
-
-          this.heightMap[i] = rowOffsetHeight
-        }
-
-        i++
-
-        if (totalHeight > scrollTop + this.viewportHeight) break
-      }
-
-      this.end = i
-
-      const remaining = listLength - this.end
-      this.averageHeight = totalHeight / this.end
-
-      this.bottom = remaining * this.averageHeight
-    },
-    async initVisibleRows() {
-      const scrollTop = this.$refs.viewport[this.getProp.scroll]
-
-      await this.$nextTick()
-
-      let content_height = this.top - scrollTop
-      let i = this.start
-
-      while (content_height < this.viewportHeight && i < this.list.length) {
-        let row = this.rows[i - this.start]
-
-        if (!row) {
-          this.end = i + 1
-          await this.$nextTick()
-          row = this.rows[i - this.start]
-        }
-
-        const rowOffsetHeight = row[this.getProp.offsetSize]
-
-        const row_height = (this.heightMap[i] = rowOffsetHeight)
-
-        content_height += row_height
-
-        i += 1
-      }
-
-      const remaining = this.list.length - this.end
-      this.averageHeight = (this.top + content_height) / this.end
-
-      this.bottom = remaining * this.averageHeight
-      this.heightMap.length = this.list.length
-    },
-
-    onStartDrag(value) {
-      this.hasStarted = true
-      this.activeIndex = value.index
-      this.moveInstance = value
-    },
-
-    onUpdate(y) {
-      if (this.hasStarted) {
-        const targetHeight =
-          this.moveInstance.targetBound[this.getProp.targetSize]
-        let offset = this.offset + y + targetHeight
-
-        let newIndex
-
-        let top = this.top
-
-        this.$refs.logger.innerHTML = `<b>${offset}</b>`
-
-        for (let i = this.start; i < this.end; i++) {
-          const height = this.heightMap[i]
-          const nextHeight = this.heightMap[i + 1] || 0
-
-          if (offset <= top + height + nextHeight / 2) {
-            newIndex = i
-            break
-          }
-
-          top += height
-        }
-
-        if (newIndex !== undefined) {
-          this.newIndex = newIndex
-        }
-      }
-    },
-
-    onEnd() {
-      if (this.newIndex !== -1) {
-        this.save = {
-          index: this.activeIndex,
-          newIndex: this.newIndex,
-        }
-        let heightMap = this.heightMap.slice()
-
-        this.heightMap = heightMap.move(this.activeIndex, this.newIndex)
-
-        this.$emit('sort', {
-          index: this.activeIndex,
-          newIndex: this.newIndex,
-        })
-      }
-
-      this.onScroll()
-
-      this.activeIndex = -1
-      this.newIndex = -1
-      this.hasStarted = false
-    },
-  },
-  computed: {
-    visible() {
-      return this.list.slice(this.start, this.end).map((data, i) => ({
-        index: i + this.start,
-        data,
-      }))
-    },
-    getContentStyle() {
-      const { paddingTop, paddingBottom } = this.getProp
-
-      return {
-        [paddingTop]: `${this.top}px`,
-        [paddingBottom]: `${this.bottom}px`,
-      }
-    },
-
-    getContainer() {
-      return this.viewport ? document.documentElement : this.$refs.viewport
-    },
-
-    getProp() {
-      const classMap = {
-        [DIRECTION.ROW]: {
-          innerClass: 'vList__inner--row',
-          contentClass: 'vList__content--row',
-          offsetSize: 'offsetWidth',
-          paddingTop: 'paddingLeft',
-          paddingBottom: 'paddingRight',
-          scroll: 'scrollLeft',
-          targetSize: 'width',
-        },
-        [DIRECTION.COLUMN]: {
-          innerClass: 'vList__inner--column',
-          contentClass: 'vList__content--column',
-          offsetSize: 'offsetHeight',
-          paddingTop: 'paddingTop',
-          paddingBottom: 'paddingBottom',
-          scroll: 'scrollTop',
-          targetSize: 'height',
-        },
-      }
-
-      return classMap[this.direction]
-    },
-  },
+  bottom.value = remaining * averageHeight.value
+  heightMap.length = props.list.length
 }
+
+const resized = () => {
+  let height = window.innerHeight || document.documentElement.offsetHeight
+  if (height !== viewportHeight.value) {
+    viewportHeight.value = height
+  }
+}
+
+const onScroll = async () => {
+  let scrollTop = 0
+
+  if (!props.viewport) {
+    scrollTop = viewport.value[getProp.value.scroll]
+  } else {
+    scrollTop = Math.max(0, -viewport.value.getBoundingClientRect().top || 0)
+  }
+
+  offset.value = scrollTop
+
+  const listLength = props.list.length
+
+  // Get start
+  let i = 0
+  let totalHeight = 0
+
+  while (i < listLength) {
+    const rowHeight = heightMap[i] || averageHeight.value
+
+    if (totalHeight + rowHeight > scrollTop) {
+      start.value = i
+
+      top.value = totalHeight
+      break
+    }
+
+    totalHeight += rowHeight
+    i += 1
+  }
+
+  while (i < listLength) {
+    if (heightMap[i]) {
+      totalHeight += heightMap[i]
+    } else if (rows.value[i]) {
+      totalHeight += rows.value[i][getProp.value.offsetSize]
+    } else {
+      end.value = i + 1
+      await nextTick()
+
+      const rowOffsetHeight =
+        rows.value[i - start.value][getProp.value.offsetSize]
+
+      totalHeight += rowOffsetHeight
+
+      heightMap[i] = rowOffsetHeight
+    }
+
+    i++
+
+    if (totalHeight > scrollTop + viewportHeight.value) break
+  }
+
+  end.value = i
+
+  const remaining = listLength - end.value
+  averageHeight.value = totalHeight / end.value
+
+  bottom.value = remaining * averageHeight.value
+}
+
+const onStartDrag = (value) => {
+  Object.assign(moveInstance, value)
+
+  hasStarted.value = true
+  activeIndex.value = value.index
+}
+
+const onUpdate = (y) => {
+  if (hasStarted.value) {
+    const targetHeight = moveInstance.targetBound[getProp.value.targetSize]
+
+    let offset = offset.value + y + targetHeight
+
+    let newIndex
+
+    let top = top.value
+
+    logger.value.innerHTML = `<b>${offset}</b>`
+
+    for (let i = start.value; i < end.value; i++) {
+      const height = heightMap[i]
+      const nextHeight = heightMap[i + 1] || 0
+
+      if (offset <= top + height + nextHeight / 2) {
+        newIndex = i
+        break
+      }
+
+      top += height
+    }
+
+    if (newIndex !== undefined) {
+      newIndex.value = newIndex
+    }
+  }
+}
+
+const onEnd = () => {
+  if (newIndex.value !== -1) {
+    let heightMap = heightMap.slice()
+
+    heightMap = reactive(heightMap.move(this.activeIndex, this.newIndex))
+
+    emit('sort', {
+      index: activeIndex.value,
+      newIndex: newIndex.value,
+    })
+  }
+
+  onScroll()
+
+  activeIndex.value = -1
+  newIndex.value = -1
+  hasStarted.value = false
+}
+
+const visible = computed(() => {
+  return props.list.slice(start.value, end.value).map((data, i) => ({
+    index: i + start.value,
+    data,
+  }))
+})
+
+const getContentStyle = computed(() => {
+  const { paddingTop, paddingBottom } = getProp.value
+
+  return {
+    [paddingTop]: `${top.value}px`,
+    [paddingBottom]: `${bottom.value}px`,
+  }
+})
+
+const getContainer = computed(() => {
+  return props.viewport ? document.documentElement : viewport.value
+})
+
+const getProp = computed(() => {
+  const classMap = {
+    [DIRECTION.ROW]: {
+      innerClass: 'vList__inner--row',
+      contentClass: 'vList__content--row',
+      offsetSize: 'offsetWidth',
+      paddingTop: 'paddingLeft',
+      paddingBottom: 'paddingRight',
+      scroll: 'scrollLeft',
+      targetSize: 'width',
+    },
+    [DIRECTION.COLUMN]: {
+      innerClass: 'vList__inner--column',
+      contentClass: 'vList__content--column',
+      offsetSize: 'offsetHeight',
+      paddingTop: 'paddingTop',
+      paddingBottom: 'paddingBottom',
+      scroll: 'scrollTop',
+      targetSize: 'height',
+    },
+  }
+
+  return classMap[props.direction]
+})
 </script>
 
 <style>
